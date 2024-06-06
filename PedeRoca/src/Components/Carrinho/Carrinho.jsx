a8a700cbd25f733aceaae90877e2935c02374a69
HugoFrajacomo <hugofrajacomo@fatec.sp.gov.br>
2024-06-05 21:11:51 -0300

    Melhorias Carrinho de compras

--- PedeRoca/src/Components/Carrinho/Carrinho.jsx
+++ PedeRoca/src/Components/Carrinho/Carrinho.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Carrinho.module.css";
 import { useState } from "react";
 
 const Carrinho = () => {
   const [step, setStep] = useState(1);
   const [selectedOption, setSelectedOption] = useState(null);
+  const [produtos, setProdutos] = useState([
+    { nome: "Alface", preco: 2.5, quantidade: 1 },
+    { nome: "Cenoura", preco: 3.2, quantidade: 2 },
+    { nome: "Tomate", preco: 1.5, quantidade: 1 },
+    { nome: "Cebola", preco: 4.5, quantidade: 1 },
+    { nome: "Batata", preco: 9.5, quantidade: 1 },
+  ]);
 
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value);
@@ -42,33 +49,36 @@ const Carrinho = () => {
       {step === 1 && (
         <div>
           <h5 className={styles.tituloCarrinho}>Itens do Carrinho</h5>
-          <div className={styles.containerCarrinho}>
-            <button className={styles.lixeira}>
-              <i className="bi bi-trash" id={styles.lixeira}></i>
-            </button>
-            <span>
-              <i className="bi bi-dash" id={styles.minus}></i>
-            </span>
-            <span>1</span>
-            <span>
-              <i className="bi bi-plus" id={styles.add}></i>
-            </span>
-            <span className={styles.nomeProduto}>Alface</span>
-            <span className={styles.precoProduto}>R$ 1,50</span>
-          </div>
-          <div className={styles.containerCarrinho}>
-            <button className={styles.lixeira}>
-              <i className="bi bi-trash" id={styles.lixeira}></i>
-            </button>
-            <span>
-              <i className="bi bi-dash" id={styles.minus}></i>
-            </span>
-            <span>2</span>
-            <span>
-              <i className="bi bi-plus" id={styles.add}></i>
-            </span>
-            <span className={styles.nomeProduto}>Cenoura </span>
-            <span className={styles.precoProduto}>R$ 1,50</span>
+          {produtos.map((produto) => (
+            <div className={styles.containerCarrinho}>
+              <button className={styles.lixeira}>
+                <i className="bi bi-trash" id={styles.lixeira}></i>
+              </button>
+              <span>
+                <i className="bi bi-dash" id={styles.minus}></i>
+              </span>
+              <span className={styles.qtdProduto}>{produto.quantidade}</span>
+              <span>
+                <i className="bi bi-plus" id={styles.add}></i>
+              </span>
+              <span className={styles.nomeProduto}>{produto.nome}</span>
+              <span className={styles.precoProduto}>
+                R$ {(produto.preco * produto.quantidade).toFixed(2)}
+              </span>
+            </div>
+          ))}
+          <div className={styles.totalCarrinho}>
+            <p className={styles.precoTotal}>
+              <span className={styles.total}>Valor </span>
+              R${" "}
+              {produtos
+                .reduce(
+                  (total, produto) =>
+                    total + produto.preco * produto.quantidade,
+                  0
+                )
+                .toFixed(2)}
+            </p>
           </div>
         </div>
       )}
@@ -91,43 +101,69 @@ const Carrinho = () => {
             <span className={styles.endLabel}>Numero</span>
             <p className={styles.endCampo}>48</p>
           </div>
-          <button id={styles.btnAlterar}><i className="bi bi-journal-medical"></i> Alterar</button>
+          <button id={styles.btnAlterar}>
+            <i className="bi bi-journal-medical"></i> Alterar
+          </button>
           <h5 className={styles.tituloCarrinho}>Tipo de entrega</h5>
           <div className={styles.containerTipoEntrega}>
             <label className={styles.checkboxLabel}>
-              <input type="radio" name="Entrega Economica" value="Economica" onClick={handleOptionChange}/> Econîmica (1 a 3 dias)
+              <input
+                type="radio"
+                name="Entrega Economica"
+                value="Economica"
+                onClick={handleOptionChange}
+              />{" "}
+              Econîmica (1 a 3 dias)
             </label>
-              <p className={styles.precoCkb}>R$ 4,00</p>
+            <p className={styles.precoCkb}>R$ 4,00</p>
           </div>
           <div className={styles.containerTipoEntrega}>
             <label className={styles.checkboxLabel}>
-              <input type="radio" name="Entrega Economica" value="Rapida" onClick={handleOptionChange}/> Rápida (máximo 50 minutos)
+              <input
+                type="radio"
+                name="Entrega Economica"
+                value="Rapida"
+                onClick={handleOptionChange}
+              />{" "}
+              Rápida (máximo 50 minutos)
             </label>
-              <p className={styles.precoCkb}>R$ 15,00</p>
+            <p className={styles.precoCkb}>R$ 15,00</p>
           </div>
           <div className={styles.containerTipoEntrega}>
             <label className={styles.checkboxLabel}>
-              <input type="radio" name="Entrega Economica" value="Agendada" onClick={handleOptionChange}/> Agendada
+              <input
+                type="radio"
+                name="Entrega Economica"
+                value="Agendada"
+                onClick={handleOptionChange}
+              />{" "}
+              Agendada
             </label>
-              <p className={styles.precoCkb}>R$ 6,00</p>
+            <p className={styles.precoCkb}>R$ 6,00</p>
           </div>
         </div>
       )}
       {step === 3 && selectedOption && (
         <div>
           <h5 className={styles.tituloCarrinho}>Forma de pagamento</h5>
-        </div>  
+        </div>
       )}
       {step === 4 && selectedOption && (
         <div>
           <h5 className={styles.tituloCarrinho}>Finalização</h5>
-        </div>  
+        </div>
       )}
       <div className={styles.containerBtn}>
-        <button className={styles.btnBack} onClick={() => setStep(step > 1 ? step - 1 : step)}>
+        <button
+          className={styles.btnBack}
+          onClick={() => setStep(step > 1 ? step - 1 : step)}
+        >
           <i className="bi bi-arrow-return-left"></i>
         </button>
-        <button className={styles.btnNext} onClick={() => setStep(step < 4 ? step + 1 : step)}>
+        <button
+          className={styles.btnNext}
+          onClick={() => setStep(step < 4 ? step + 1 : step)}
+        >
           <i className="bi bi-check-lg"></i>
         </button>
       </div>
