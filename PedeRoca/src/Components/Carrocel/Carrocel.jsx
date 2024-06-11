bf08392b6d8188094f300e9907d86562826be342
HugoFrajacomo <hugofrajacomo@fatec.sp.gov.br>
2024-06-11 13:38:40 -0300

    Carrocel com bootstrap

--- PedeRoca/src/Components/Carrocel/Carrocel.jsx
+++ PedeRoca/src/Components/Carrocel/Carrocel.jsx
@@ -1,30 +1,22 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import ImgCarrocel1 from '../../assets/Carrocel1.svg';
-import ImgCarrocel2 from '../../assets/Carrocel2.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './Carrocel.module.css';
-import { register } from 'swiper/element/bundle'
-register();
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/scrollbar'
-import 'swiper/css/autoplay'
-
+import { Carousel } from "react-bootstrap";
+import ImgCarrocel1 from "../../assets/Carrocel1.svg";
+import ImgCarrocel2 from "../../assets/Carrocel2.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./Carrocel.module.css";
 
 const Carrocel = () => {
-    return (
-        <div>
-            <Swiper
-                slidesPerView={1}
-                pagination={{ clickable: true }}
-                autoplay={{ delay: 4000 }}
-            >
-                <SwiperSlide><img src={ImgCarrocel1} alt="imagem carrocel 1" className="d-block w-100 mt-2 d-md-none" /></SwiperSlide>
-                <SwiperSlide><img src={ImgCarrocel2} alt="imagem carrocel 2" className="d-block w-100 mt-2 d-md-none" /></SwiperSlide>
-            </Swiper>
-        </div>
-    );
+  return (
+    <>
+      <Carousel className="d-md-none mt-2">
+        <Carousel.Item>
+          <img className="w-100" src={ImgCarrocel1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="w-100" src={ImgCarrocel2} alt="Second slide" />
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
 };
 
 export default Carrocel;
