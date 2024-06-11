fe168cf0991779e82dd287c876567649659ce1de
HugoFrajacomo <hugofrajacomo@fatec.sp.gov.br>
2024-06-11 13:46:41 -0300

    Adição de carrocel de imagens

--- PedeRoca/src/Components/Carrocel/Carrocel.jsx
+++ PedeRoca/src/Components/Carrocel/Carrocel.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Carousel } from "react-bootstrap";
+import ImgCarrocel1 from "../../assets/Carrocel1.svg";
+import ImgCarrocel2 from "../../assets/Carrocel2.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./Carrocel.module.css";
 
 const Carrocel = () => {
-  return <div>Carrocel</div>;
+  return (
+    <>
+      <Carousel className="d-md-none mt-2">
+        <Carousel.Item>
+          <img className="w-100" src={ImgCarrocel1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="w-100" src={ImgCarrocel2} alt="Second slide" />
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
 };
 
 export default Carrocel;
