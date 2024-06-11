8c7faee8d2bbda964870184f0fb0e91c8be0258b
HugoFrajacomo <hugofrajacomo@fatec.sp.gov.br>
2024-06-11 13:45:05 -0300

    zerandoCarrocel

--- PedeRoca/src/Components/Carrocel/Carrocel.jsx
+++ PedeRoca/src/Components/Carrocel/Carrocel.jsx
@@ -1,22 +1,7 @@
-import { Carousel } from "react-bootstrap";
-import ImgCarrocel1 from "../../assets/Carrocel1.svg";
-import ImgCarrocel2 from "../../assets/Carrocel2.svg";
-import "bootstrap/dist/css/bootstrap.min.css";
-import styles from "./Carrocel.module.css";
+import React from "react";
 
 const Carrocel = () => {
-  return (
-    <>
-      <Carousel className="d-md-none mt-2">
-        <Carousel.Item>
-          <img className="w-100" src={ImgCarrocel1} alt="First slide" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="w-100" src={ImgCarrocel2} alt="Second slide" />
-        </Carousel.Item>
-      </Carousel>
-    </>
-  );
+  return <div>Carrocel</div>;
 };
 
 export default Carrocel;
