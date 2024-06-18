c031526f9d3b36614c5028b2adb06c1684f95d23
HugoFrajacomo <hugofrajacomo@fatec.sp.gov.br>
2024-06-18 16:37:51 -0300

    Finalizaçõa da interface do carrinho de compras

--- PedeRoca/src/Components/Carrinho/Carrinho.jsx
+++ PedeRoca/src/Components/Carrinho/Carrinho.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect } from "react";
 import styles from "./Carrinho.module.css";
 import { useState } from "react";
+import pix from "../../assets/pix.svg";
+import boleto from "../../assets/boleto.svg";
+import card from "../../assets/card.svg";
 
 const Carrinho = () => {
   const [step, setStep] = useState(1);
   const [selectedOption, setSelectedOption] = useState(null);
+  const [avisoEntrega, setAvisoEntrega] = useState(false);
+  const [selectedPayment, setSelectedPayment] = useState(null);
+  const [avisoPagamento, setAvisoPagamento] = useState(false);
   const [produtos, setProdutos] = useState([
     { nome: "Alface", preco: 2.5, quantidade: 1 },
     { nome: "Cenoura", preco: 3.2, quantidade: 2 },
@@ -16,6 +22,28 @@ const Carrinho = () => {
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value);
   };
+
+  const handlePaymentChange = (event) => {
+    setSelectedPayment(event.target.value);
+  };
+
+  useEffect(() => {
+    if (step === 3 && !selectedOption) {
+      setAvisoEntrega(true);
+      setStep(2);
+    } else if (step === 3 && selectedOption) {
+      setAvisoEntrega(false);
+    }
+  }, [selectedOption, step]);
+
+  useEffect(() => {
+    if (step === 4 && !selectedPayment) {
+      setAvisoPagamento(true);
+      setStep(3);
+    } else if (step === 4 && selectedPayment) {
+      setAvisoPagamento(false);
+    }
+  }, [selectedPayment, step]);
 
   return (
     <>
@@ -112,6 +140,7 @@ const Carrinho = () => {
                 name="Entrega Economica"
                 value="Economica"
                 onClick={handleOptionChange}
+                checked={selectedOption === "Economica"}
               />{" "}
               Econômico (1 a 3 dias)
             </label>
@@ -124,6 +153,7 @@ const Carrinho = () => {
                 name="Entrega Economica"
                 value="Rapida"
                 onClick={handleOptionChange}
+                checked={selectedOption === "Rapida"}
               />{" "}
               Rápida (máximo 50 minutos)
             </label>
@@ -136,22 +166,100 @@ const Carrinho = () => {
                 name="Entrega Economica"
                 value="Agendada"
                 onClick={handleOptionChange}
+                checked={selectedOption === "Agendada"}
               />{" "}
               Agendada
             </label>
             <p className={styles.precoCkb}>R$ 6,00</p>
           </div>
+          {avisoEntrega && (
+            <p className={styles.aviso}>Selecione uma forma de entrega</p>
+          )}
         </div>
       )}
       {step === 3 && selectedOption && (
-        <div>
+        <>
           <h5 className={styles.tituloCarrinho}>Forma de pagamento</h5>
-        </div>
+          <section className={styles.containerPagamento}>
+            <input
+              type="radio"
+              name="pagamento"
+              value="Cartão"
+              onClick={handlePaymentChange}
+              checked={selectedPayment === "Cartão"}
+            />
+            <div>
+              <img src={card} alt="Cartão" className={styles.iconePagamento} />
+              <p className={styles.textoPagamento}>Cartão</p>
+            </div>
+          </section>
+          <section className={styles.containerPagamento}>
+            <input
+              type="radio"
+              name="pagamento"
+              value="Pix"
+              onClick={handlePaymentChange}
+              checked={selectedPayment === "Pix"}
+            />
+            <div>
+              <img src={pix} alt="Pix" className={styles.iconePagamento} />
+              <p className={styles.textoPagamento}>Pix</p>
+            </div>
+          </section>
+          <section className={styles.containerPagamento}>
+            <input
+              type="radio"
+              name="pagamento"
+              value="Boleto"
+              onClick={handlePaymentChange}
+              checked={selectedPayment === "Boleto"}
+            />
+            <div>
+              <img
+                src={boleto}
+                alt="boleto bancário"
+                className={styles.iconePagamento}
+              />
+              <p className={styles.textoPagamento}>Boleto</p>
+            </div>
+          </section>
+          {avisoPagamento && (
+            <p className={styles.aviso}>Selecione um método de pagamento</p>
+          )}
+        </>
       )}
-      {step === 4 && selectedOption && (
-        <div>
+      {step === 4 && selectedOption && selectedPayment && (
+        <>
           <h5 className={styles.tituloCarrinho}>Finalização</h5>
-        </div>
+          <h5 className={styles.sumario}>Sumário</h5>
+          <div className={styles.contFinalizacao}>
+            <p>Produtos</p>
+            <span>R$ 11,00</span>
+          </div>
+          <div className={styles.contFinalizacao}>
+            <p>Frete</p>
+            <span>R$ 5,00</span>
+          </div>
+          <div className={styles.contFinalizacao}>
+            <p>Total</p>
+            <span>R$ 16,00</span>
+          </div>
+          <div>
+            <p className={styles.textoNome}>Gabriel Pelicolla</p>
+            <p className={styles.texto}>
+              Rua 9 de julho, centro, 485, Araraquara - SP
+            </p>
+            <p className={styles.texto}>CEP: 14.800.254</p>
+          </div>
+          <div className={styles.contEndereco}>
+            <p>gpelicolla@gmail.com</p>
+            <p>CPF: 455.456.977-01</p>
+          </div>
+          <section className={styles.especEntrega}>
+            <p>Expectativa de entrega</p>
+            <p>Sexta-Feira - 21/06/2024</p>
+          </section>
+        </>
       )}
       <div className={styles.containerBtn}>
         <button
