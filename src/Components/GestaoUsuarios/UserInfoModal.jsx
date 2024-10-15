854c33d299cb9468e1be1ece7eced1c4f2c695ae
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-10-15 16:30:48 -0300

    #update mudança no layout UserInfoModal

--- src/Components/GestaoUsuarios/UserInfoModal.jsx
+++ src/Components/GestaoUsuarios/UserInfoModal.jsx
@@ -4,27 +4,74 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import styles from './UserInfoModal.module.css';
 
 const UserInfoModal = ({ show, handleClose, usuario }) => {
+  const [isEditable, setIsEditable] = React.useState(false);
+  
+  const toggleEdit = () => {
+    setIsEditable(!isEditable);
+  };
+
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Informações do Usuário</Modal.Title>
+        <Modal.Title><p className={styles.TituloDetalhes}>Detalhes do Usuário</p></Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <div className={styles.userInfo}>
-          <p><strong>Nome:</strong> {usuario.nome}</p>
-          <p><strong>Email:</strong> {usuario.email}</p>
-          <p><strong>Status:</strong> {usuario.status}</p>
-          <p><strong>Telefone:</strong> {usuario.telefone || "Não informado"}</p>
-          <p><strong>Endereço:</strong> {usuario.endereco || "Não informado"}</p>
-          {/* Adicione outros atributos conforme necessário */}
+          <label className={styles.label}>Nome: 
+            <input 
+              type="text" 
+              value={usuario.nome} 
+              disabled={!isEditable} 
+              className={styles.inputField} // Use um estilo adequado
+            />
+          </label>
+          <br />
+          <label className={styles.label}> Email: 
+            <input 
+              type="email" 
+              value={usuario.email} 
+              disabled={!isEditable} 
+              className={styles.inputField}
+            />
+          </label>
+          <br />
+          <label className={styles.label}>Status: 
+            <input 
+              type="text" 
+              value={usuario.status} 
+              disabled={!isEditable} 
+              className={styles.inputField}
+            />
+          </label>
+          <br />
+          <label className={styles.label}> Telefone:
+            <input 
+              type="text" 
+              value={usuario.telefone || "Não informado"} 
+              disabled={!isEditable} 
+              className={styles.inputField}
+            />
+          </label>
+          <br />
+          <label className={styles.label}>
+            <strong>Endereço:</strong>
+            <input 
+              type="text" 
+              value={usuario.endereco || "Não informado"} 
+              disabled={!isEditable} 
+              className={styles.inputField}
+            />
+          </label>
         </div>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose} className={styles.Voltar}>
+        <Button onClick={handleClose} className={styles.Voltar}>
           Voltar
         </Button>
-        <Button variant="primary" onClick={handleClose} className={styles.Editar}>
-          Editar
+        <Button 
+          onClick={toggleEdit} 
+          className={styles.Editar}>
+          {isEditable ? "Salvar" : "Editar"}
         </Button>
       </Modal.Footer>
     </Modal>
