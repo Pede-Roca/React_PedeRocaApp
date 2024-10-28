1e896ca3fee11fbca643e553f8e4de7d6f315386
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-10-28 15:30:15 -0300

    #update Cards de Recompra

--- src/Components/Usuario/UserCompras.jsx
+++ src/Components/Usuario/UserCompras.jsx
@@ -1,7 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import styles from "./Usuario.module.css";
+import { buscarHistoricoDeCompraPorId } from '../../services';
+import { useAuth } from './useAuth';
 
-export const UserCompras = () => {
+export const UserCompras = ({ onBack }) => {
+  const [Carrinhos, setCarrinhos] = useState([]);
+  const { backendUserId } = useAuth();
+
+  useEffect(() => {
+    const fetchCarrinhos = async () => {
+      const histCarrinho = await buscarHistoricoDeCompraPorId(backendUserId);
+      const ultimosCarrinhos = histCarrinho.slice(-4);
+      setCarrinhos(ultimosCarrinhos);
+    };
+    fetchCarrinhos();
+  }, [backendUserId]);
+
   return (
-    <div>UserCompras</div>
-  )
-}
+    <>
+      {Carrinhos.length > 0 ? (
+        <>
+          {Carrinhos.map((carrinho, i) => {
+            const valorTotal = carrinho.itensCarrinhoCompra.reduce((total, item) => {
+              return total + item.preco * item.quantidade;
+            }, 0);
+
+            const allItemsInStock = carrinho.itensCarrinhoCompra.every(
+              item => item.quantidade <= item.estoque
+            );
+
+            return (
+              <div key={i} className={styles.carrinhoItem}>
+                <div className={styles.containerHist}>
+                  <p><strong>Data:</strong> {new Date(carrinho.data).toLocaleDateString()}</p>
+                  <p><strong>Status:</strong> {carrinho.status ? "Ativo" : "Finalizado"}</p>
+                </div>
+                <div className={styles.itensCarrinho}>
+                  <table className={styles.table}>
+                    <thead>
+                      <tr className={styles.cabecalhoTabela}>
+                        <th>Produto</th>
+                        <th>Qtd</th>
+                        <th>Preço (R$)</th>
+                        <th className={styles.ocultarColuna}>Estoque</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {carrinho.itensCarrinhoCompra.map((item, index) => (
+                        <tr key={index} className={styles.conteudoTabela}>
+                          <td>{item.nomeProduto.length > 15 ? item.nomeProduto.substring(0, 12) + "..." : item.nomeProduto}</td>
+                          <td>{item.quantidade}</td>
+                          <td>{item.preco.toFixed(2)}</td>
+                          <td className={styles.ocultarColuna}>{item.estoque}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  <div className="d-flex justify-content-center">
+                    <span className={styles.TotalValor}>R$ {valorTotal.toFixed(2)}</span>
+                    <button
+                      className={styles.btnRecompra}
+                      disabled={!allItemsInStock}
+                    >
+                      Adicionar ao Carrinho
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <div>
+          <h5>Nenhuma compra realizada</h5>
+          <p>Realize uma compra para ter acesso ao seu histórico de compras.</p>
+        </div>
+      )}
+      <button onClick={onBack} className={styles.btnBack}>
+        <i className="bi bi-arrow-return-left"></i>
+      </button>
+    </>
+  );
+};
