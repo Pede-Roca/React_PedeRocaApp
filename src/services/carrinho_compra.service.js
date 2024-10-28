37a7c85163d8aa776751962b73e00534d3bae7f3
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-10-28 08:53:31 -0300

    #update Reconhecimento da lista de compras do usuário

--- src/services/carrinho_compra.service.js
+++ src/services/carrinho_compra.service.js
@@ -32,6 +32,10 @@ export const criarCarrinhoDeCompraNoBackend = async (idUsuario) => {
 };
 
 // Busca o carrinho de compra pelo ID do usuário no backend
+export const buscarCarrinhoDeCompraPeloIdDoUsuarioNoBackend = async (idUsuario) => {
+    const { data, erro } = await realizarRequisicao("get", `${import.meta.env.VITE_API_URL}carrinho-compra/buscar-por-id-usuario/${idUsuario}`);
+    return erro ? null : data.id;
+};
 
 
 // Adiciona um produto ao carrinho de compra
