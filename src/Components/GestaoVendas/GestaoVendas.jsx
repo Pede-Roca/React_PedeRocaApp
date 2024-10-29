c275b294c14954ca5adedba6cd24ab87ab903267
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-10-28 22:35:21 -0300

    #update Abas da gestão de vendas

--- src/Components/GestaoVendas/GestaoVendas.jsx
+++ src/Components/GestaoVendas/GestaoVendas.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import styles from './GestaoVendas.module.css'
 
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
 const GestaoVendas = () => {
   return (
-    <div>GestaoVendas</div>
+    <>
+    <Tabs
+      defaultActiveKey="Pedidos"
+      id="fill-tab-example"
+      className={styles.tabs}
+      fill
+    >
+      <Tab eventKey="Pedidos" title="Pedidos">
+        <h1>Colocar componente de lista de Pedidos</h1>
+      </Tab>
+      <Tab eventKey="Aberto" title="Vendas - Aberto">
+        <h1>Colocar componente de lista de Acompanhamento de Vendas</h1>
+      </Tab>
+      <Tab eventKey="Entrega" title="Vendas - Entrega">
+        <h1>A definir</h1>
+      </Tab>
+      <Tab eventKey="Finalizadas" title="Vendas - Finalizadas">
+         <h1>A definir</h1>
+      </Tab>
+    </Tabs>
+    </>
   )
 }
 
