b36de29aca50b80bb0869a1bbe8df0629ffb47d5
SamaraPalladino <luizamaradelg@gmail.com>
2024-11-12 20:44:14 -0300

    reajustando

--- src/services/usuario.services.js
+++ src/services/usuario.services.js
@@ -61,14 +61,4 @@ export const buscarTodosUsuariosNoBackend = async () => {
     }
 }
 
-export const atualizarNivelUsuarioNoBackend = async (userId, novoNivel) => {
-    try {
-        const { data } = await axios.put(`${import.meta.env.VITE_API_URL}usuario/atualizar-nivel-acesso/${userId}`, {
-            nivelAcesso : novoNivel
-        });
 
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
-}
